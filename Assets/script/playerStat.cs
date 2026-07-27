441c3d734c919c2cad5b4af91140793005709786
DracoMasque <aksel.boulongne@gmail.com>
2026-07-27 20:06:59 +0200

    CR2ATION PLAYER STAT

---
+++ Assets/script/playerStat.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class playerStat : MonoBehaviour
+{
+    public int air_mana;
+    public int eau_mana;
+    public int feu_mana;
+    public int terre_mana;
+    public int temps_mana;
+    public int vide_mana;
+    public int espace_mana;
+    public int placeHolder_mana;
+    public int placeHolder2_mana;
+    
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
