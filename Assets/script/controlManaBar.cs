6e75498116fdf019d6979fcff2a67fed3a997fed
DracoMasque <aksel.boulongne@gmail.com>
2026-08-11 12:52:59 +0200

    camera réparé

--- Assets/script/controlManaBar.cs
+++ Assets/script/controlManaBar.cs
@@ -1,85 +1,53 @@
+using System;
+using System.Diagnostics;
 using TMPro;
 using UnityEngine;
+using UnityEngine.Serialization;
 using UnityEngine.UI;
 
-public class controlManaBar : MonoBehaviour
+public class ControlManaBar : MonoBehaviour
 {
-    [SerializeField] private type_mana type_mana;
+    [FormerlySerializedAs("type_mana")] [SerializeField] private TypeMana typeMana;
     [SerializeField] private playerStat player;
-    private TextMeshProUGUI text;
+    private TextMeshProUGUI _text;
     [SerializeField] private Image image;
+    public float typeManaAmount;
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player").GetComponent<playerStat>();
-        text = GetComponentInChildren<TextMeshProUGUI>();
+        _text = GetComponentInChildren<TextMeshProUGUI>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (type_mana == type_mana.air_mana)
+        typeManaAmount = typeMana switch
         {
-            float ratio = player.air_mana/player.max_mana;
-            print(player.air_mana/player.max_mana);
-            text.text = player.air_mana.ToString();
-            image.fillAmount = ratio;
-        }
-        else if (type_mana == type_mana.eau_mana)
-        {
-            float ratio = player.eau_mana/player.max_mana;
-            text.text = player.eau_mana.ToString();
-            image.fillAmount = ratio;
-        }
-        else if (type_mana == type_mana.feu_mana)
-        {
-            float ratio = player.feu_mana/player.max_mana;
-            text.text = player.feu_mana.ToString();
-            image.fillAmount = ratio;
-        }
-        else if (type_mana == type_mana.terre_mana)
-        {
-            float ratio = player.terre_mana/player.max_mana;
-            text.text = player.terre_mana.ToString();
-            image.fillAmount = ratio;
-        }
-        else if (type_mana == type_mana.temps_mana)
-        {
-            float ratio = player.temps_mana/player.max_mana;
-            text.text = player.temps_mana.ToString();
-            image.fillAmount = ratio;
-        }
-        else if (type_mana == type_mana.vide_mana)
-        {
-            float ratio = player.vide_mana/player.max_mana;
-            text.text = player.vide_mana.ToString();
-            image.fillAmount = ratio;
-        }
-        else if (type_mana == type_mana.espace_mana)
-        {
-            float ratio = player.espace_mana/player.max_mana;
-            text.text = player.espace_mana.ToString();
-            image.fillAmount = ratio;
-        }
-        else if (type_mana == type_mana.placeHolder_mana)
-        {
-            float ratio = player.placeHolder_mana/player.max_mana;
-            text.text = player.placeHolder_mana.ToString();
-            image.fillAmount = ratio;
-        }
+            TypeMana.AirMana => player.air_mana,
+            TypeMana.EauMana => player.eau_mana,
+            TypeMana.FeuMana => player.feu_mana,
+            TypeMana.TerreMana => player.terre_mana,
+            TypeMana.TempsMana => player.temps_mana,
+            TypeMana.VideMana => player.vide_mana,
+            TypeMana.EspaceMana => player.espace_mana,
+            TypeMana.PlaceHolderMana => player.placeHolder_mana
+        };
+        _text.text = typeManaAmount.ToString();
+        image.fillAmount = typeManaAmount/player.max_mana;
         
     }
 }
 
-enum type_mana
+enum TypeMana
 {
-    air_mana,
-    eau_mana,
-    feu_mana,
-    terre_mana,
-    temps_mana,
-    vide_mana,
-    espace_mana,
-    placeHolder_mana
+    AirMana,
+    EauMana,
+    FeuMana,
+    TerreMana,
+    TempsMana,
+    VideMana,
+    EspaceMana,
+    PlaceHolderMana
 }
--- Assets/script/moveCam.cs
+++ Assets/script/moveCam.cs
@@ -2,57 +2,55 @@ using Unity.Cinemachine;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-public class moveCam : MonoBehaviour
+public class MoveCam : MonoBehaviour
 {
     [SerializeField] private float speed;
     [SerializeField] private int screenEdge;
-    private Vector2 moveInput;
-    private Rigidbody2D rb;
+    private Vector2 _moveInput;
+    private Rigidbody2D _rb;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        rb = GetComponent<Rigidbody2D>();
+        _rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
-        
-        rb.linearVelocity = moveInput.normalized * speed;
+        _rb.linearVelocity = _moveInput.normalized * speed;
     }
 
     public void Move(InputAction.CallbackContext ctx)
     {
-        moveInput = ctx.ReadValue<Vector2>();
+        _moveInput = ctx.ReadValue<Vector2>();
     }
     
     public void EdgeMove(InputAction.CallbackContext ctx)
     {
         if (ctx.ReadValue<Vector2>().y < screenEdge)
         {
-            moveInput.y = -1f;
+            _moveInput.y = -1f;
         }
         else if (ctx.ReadValue<Vector2>().y > Screen.height - screenEdge)
         {
-            moveInput.y = +1f;
+            _moveInput.y = +1f;
         }
         
         if (ctx.ReadValue<Vector2>().x < screenEdge)
         {
-            moveInput.x = -1f;
+            _moveInput.x = -1f;
         }
         else if (ctx.ReadValue<Vector2>().x > Screen.width - screenEdge)
         {
-            moveInput.x = +1f;
+            _moveInput.x = +1f;
         }
 
         if (ctx.ReadValue<Vector2>().y > screenEdge && ctx.ReadValue<Vector2>().y < Screen.height - screenEdge &&
             ctx.ReadValue<Vector2>().x > screenEdge && ctx.ReadValue<Vector2>().x < Screen.width - screenEdge)
         {
-            moveInput.x = 0f;
-            moveInput.y = 0f;
+            _moveInput.x = 0f;
+            _moveInput.y = 0f;
         }
     }
     
