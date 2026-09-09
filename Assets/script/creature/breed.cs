f54f4453fd9f8a530ee771162de7125907e99399
aksel <aksel.boulongne@gmail.com>
2026-09-09 09:34:47 +0200

    avancement breed script

--- Assets/script/creature/breed.cs
+++ Assets/script/creature/breed.cs
@@ -52,6 +52,67 @@ public class Breed
 
     public void PickActiveGenes(List<Gene> enfant)
     {
+        List<Gene> checkingGenes = new List<Gene>();
+        List<Gene> activeGenes = new List<Gene>();
+        bool geneFound = false;
+        int i = 0;
+        
+            
+        foreach (Gene g in enfant)
+        {
+            for (int g2=1; g2<=enfant.Count; g2++)
+            {
+                    if (g.geneType == enfant[g2].geneType)
+                    {
+                        checkingGenes.Add(enfant[g2]);
+                    }
+            }
+
+            if (checkingGenes.Count > 1)
+            {
+                while (!geneFound)
+                {
+                    List<Gene> dominantGene = new List<Gene>();
+                    List<Gene> possibleGene = new List<Gene>();
+                    foreach (Gene g3 in checkingGenes)
+                    {
+                        if (g3.isDominant)
+                        {
+                            dominantGene.Add(g3);
+                        }
+
+                        if (g3 == g)
+                        {
+                            possibleGene.Add(g3);
+                        }
+                    }
+
+                    if (dominantGene.Count > 1)
+                    {
+                        Gene currentGene = dominantGene[0];
+                        for (int g4 = 1; g4 <= dominantGene.Count; g4++)
+                        {
+                            if (currentGene.dominantPercentage < dominantGene[g4].dominantPercentage)
+                            {
+                                currentGene = dominantGene[g4];
+                            }
+                        }
+                        activeGenes.Add(currentGene);
+                        geneFound = true;
+                    }
+                    else if (dominantGene.Count != 0)
+                    {
+                        activeGenes.Add(dominantGene[0]);
+                        geneFound = true;
+                    }
+                }
+            }
+            else
+            {
+                activeGenes.Add(g);
+            }
+        }
+        
         
     }
 }
